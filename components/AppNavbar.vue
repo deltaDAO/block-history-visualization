048e2fd5b0ed9e2f4ae274ec480c122e7281c0e0
Abrom8 <38963270+Abrom8@users.noreply.github.com>
2022-03-25 09:27:42 +0100

    feat(layout): add footer and navbar

---
+++ components/AppNavbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="navbar w-full bg-base-100">
+    <a
+      href="https://portal.minimal-gaia-x.eu/"
+      class="btn btn-ghost normal-case"
+      rel="noopener noreferrer"
+      target="_blank"
+      >Portal
+    </a>
+
+    <a
+      href="https://academy.delta-dao.com/"
+      class="btn btn-ghost normal-case"
+      rel="noopener noreferrer"
+      target="_blank"
+      >Academy
+    </a>
+
+    <a
+      href="https://exchangelog.minimal-gaia-x.eu/"
+      class="btn btn-ghost normal-case"
+      rel="noopener noreferrer"
+      target="_blank"
+      >Blockexplorer
+    </a>
+  </nav>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="grid grid-cols-12 grid-flow-row w-full h-full container mx-auto relative"
+  >
+    <div class="grid grid-cols-12 grid-flow-col w-full h-full absolute -z-10">
+      <div class="col-start-1 col-span-1 w-full h-full bg-base-200"></div>
+      <div class="col-start-2 col-end-8 w-full h-full"></div>
+      <div class="col-start-9 col-span-4 w-full h-full md:bg-secondary"></div>
+    </div>
+    <app-navbar class="col-start-2 col-end-8" />
+    <Nuxt class="col-start-2 col-span-11 px-4" />
+    <app-footer class="col-start-1 col-span-12" />
+  </div>
+</template>
+
+<script>
+import AppFooter from '~/components/AppFooter.vue'
+import AppNavbar from '~/components/AppNavbar.vue'
+export default {
+  components: { AppNavbar, AppFooter },
+}
+</script>
