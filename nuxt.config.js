0f1373d71ff8570416fd6a287881b5e933a5fd4f
Abrom8 <38963270+Abrom8@users.noreply.github.com>
2022-03-29 15:16:20 +0200

    feat: add page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'block-history-visualization',
+    title: 'Gaia-X Testnet Statistics',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
